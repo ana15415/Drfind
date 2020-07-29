968e243aac9600182c2db9f9d0a0be825cdd1fa7
ana15415 <38575311+ana15415@users.noreply.github.com>
2020-07-29 05:06:13 -0500

    Update Medicos.java

--- app/src/main/java/com/example/drfind/Controlador/Medicos.java
+++ app/src/main/java/com/example/drfind/Controlador/Medicos.java
@@ -1,5 +1,7 @@
 package com.example.drfind.Controlador;
 
+import com.example.drfind.Controlador.adaptadores.AdaptadoresMedicos.citaslistmodelo;
+import com.example.drfind.Controlador.adaptadores.AdaptadoresMedicos.pacientemodelo;
 import com.example.drfind.Controlador.adaptadores.AdaptadoresPaciente.medicodetallemodelo;
 import com.example.drfind.Controlador.adaptadores.AdaptadoresPaciente.medicomodelo;
 import com.example.drfind.modelo.conexion;
@@ -33,73 +35,79 @@ public class Medicos {
             e.getMessage();
         }
     }
-    public void agregarhora(String usuamedico,String horas){
-        conexion db=new conexion();
-        try {
-            PreparedStatement pst=db.conexionbd().prepareStatement("insert_horario ?,?");
-            pst.setString(1,usuamedico);
-            pst.setString(2,horas);
-        } catch (SQLException e) {
-            e.getMessage();
-        }
-    }
-
-    public void ratingbar(String usuariomed, float califica){
-        conexion db=new  conexion();
-        try {
-            PreparedStatement pst=db.conexionbd().prepareStatement("ratingbar_valor ?,?");
-            pst.setString(1,usuariomed);
-            pst.setFloat(2,califica);
-            pst.executeUpdate();
-        }catch (Exception e){
-            e.getMessage();
-        }
-    }
-
-    public void eliminar(String usuariomed){
-        conexion db=new conexion();
-        try {
-            PreparedStatement pst=db.conexionbd().prepareStatement("elim_pac ?");
-            pst.setString(1,usuariomed);
-            pst.executeUpdate();
-        }catch (Exception e){
-            e.getMessage();
-        }
-    }
-
-        public List<medicomodelo> mostrarlistmedi(int idespeci){
+        public List<medicomodelo> mostrarlistmedi(String idespeci,String usuariopac){
         List<medicomodelo> medicos=new ArrayList<>();
         conexion db=new conexion();
         try {
-            PreparedStatement pst=db.conexionbd().prepareStatement("lista_med ?");
-            pst.setInt(1,idespeci);
+            PreparedStatement pst=db.conexionbd().prepareStatement("lista_med ?,?");
+            pst.setString(1,idespeci);
+            pst.setString(2,usuariopac);
             ResultSet rst=pst.executeQuery();
             while (rst.next()){
-                medicos.add(new medicomodelo(rst.getString("nombremedico"),rst.getString("nombreespecialidad"),
-                        rst.getString("telefonomed"), rst.getString("usuariomed")));
+                medicos.add(new medicomodelo(rst.getString("nombremedico"),rst.getString("apellidos"), rst.getString("usuariomed"),rst.getString("usuariopac"),rst.getFloat("calificacion")));
             }
         }catch (SQLException e){
             e.getMessage();
         }
         return medicos;
     }
-    public List<medicodetallemodelo> mostrardetallemedi(String usuariomedico){
+    public List<medicodetallemodelo> mostrardetallemedi(String usuariomedico,String usuariopac){
         List<medicodetallemodelo> medicos=new ArrayList<>();
         conexion db=new conexion();
         try {
-            PreparedStatement pst=db.conexionbd().prepareStatement("lista_detallemed ?");
+            PreparedStatement pst=db.conexionbd().prepareStatement("lista_detallemed ?,?");
             pst.setString(1,usuariomedico);
+            pst.setString(2,usuariopac);
             ResultSet rst=pst.executeQuery();
             while (rst.next()){
-                medicos.add(new medicodetallemodelo(rst.getString("nombremedico"),rst.getString("nombreespecialidad"),
-                        rst.getString("telefonomed"),rst.getString("e_mailmed"),rst.getString("usuariomed")));
+                medicos.add(new medicodetallemodelo(rst.getString("usuariomed"),rst.getString("nombremedico"),rst.getString("apellidos"),
+                        rst.getString("telefonomed"),rst.getString("e_mailmed"),rst.getString("usuariopac")));
             }
         }catch (SQLException e){
             e.getMessage();
         }
         return medicos;
     }
-
+    public List<pacientemodelo> pacientemodeloList(String usuariom){
+        List<pacientemodelo> paciente=new ArrayList<>();
+        conexion db=new conexion();
+        try {
+            PreparedStatement pst=db.conexionbd().prepareStatement("list_pacientes_med ?");
+            pst.setString(1,usuariom);
+            ResultSet rst=pst.executeQuery();
+            while (rst.next()){
+                paciente.add(new pacientemodelo(rst.getString("usuariomed"),rst.getString("usuariopac"),rst.getString("nombrepac"),rst.getString("apellidospac"),rst.getString("direccionpac"),
+                        rst.getString("telefonopac"),rst.getString("e_mailpac")));
+            }
+        }catch (SQLException e){
+            e.getMessage();
+        }
+        return paciente;
+    }
+    public List<citaslistmodelo> citaslistmodeloList(String usuariom){
+        List<citaslistmodelo> citame=new ArrayList<>();
+        conexion db=new conexion();
+        try {
+            PreparedStatement pst=db.conexionbd().prepareStatement("list_citas_medico ?");
+            pst.setString(1,usuariom);
+            ResultSet rst=pst.executeQuery();
+            while (rst.next()){
+                citame.add(new citaslistmodelo(rst.getString("usuariomed"),rst.getString("usuariopac"),rst.getString("nombrepac"),rst.getString("apellidospac"),rst.getString("fechacita"),
+                        rst.getString("horacita")));
+            }
+        }catch (SQLException e){
+            e.getMessage();
+        }
+        return citame;
+    }
+    public void eliminarpac (String usuariopac){
+        conexion db=new conexion();
+        try {
+            PreparedStatement pst=db.conexionbd().prepareStatement("elim_pac ?");
+            pst.setString(1,usuariopac);
+            pst.executeUpdate();
+        }catch (Exception e){
+            e.getMessage();
+        }
+    }
 }
-
-
